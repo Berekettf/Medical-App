e8918e13977f4df31e88fce79b8af55d2900a8d5
BereketWU <BereketWU>
2024-08-15 08:47:57 +0300

    added mega menu

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,10 +1,14 @@
+import Megamenu from '@/components/Frontend/Megamenu'
 import Navbar from '@/components/Frontend/Navbar'
 import React, { ReactNode } from 'react'
 
 export default function Layout({children} :{children:ReactNode}) {
   return (
-    <div>
+    <div className='bg-blue-950'>
       <Navbar/>
+      <div className='max-w-5xl mx-auto py-6'>
+      <Megamenu/>
+      </div>
       {children}
     </div>
   )
---
+++ notes.md
@@ -0,0 +1,6 @@
+Book Your Occupational Therapist
+
+const TEXTS = ['Message', 'Dental', 'Cosmetic', 'Speech Therapist', 'Occupational Therapist', 'Acupucturist'];
+
+Health shouldn't be a puzzle, we are cutting through the B.C to
+bring you simple affordable, and transparent healthcare
