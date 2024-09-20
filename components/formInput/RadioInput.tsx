eb213a38680d8f6a920817d620ee21b9aab393a9
Bereket <bereketwubalem19@gmail.com>
2024-09-20 13:59:45 +0300

    fixing

--- components/formInput/RadioInput.tsx
+++ components/formInput/RadioInput.tsx
@@ -29,11 +29,13 @@ export default function RadioInput({
       <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         {radioOptions.map((radio, r) => {
           return (
-            <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <li
+              key={radio.value} 
+              className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600"
+            >
               <div className="flex items-center ps-3">
                 <input
                   {...register(name, { required: true })}
-                  key={r}
                   id={radio.value}
                   type="radio"
                   value={radio.value}
@@ -41,7 +43,7 @@ export default function RadioInput({
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
                 />
                 <label
-                  htmlFor="horizontal-list-radio-license"
+                  htmlFor={radio.value}
                   className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   {radio.label}
